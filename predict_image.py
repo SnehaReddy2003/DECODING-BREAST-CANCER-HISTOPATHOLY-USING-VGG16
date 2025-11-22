7c128fe1b29c69a4d8632bdbbc9682134cc7d75d
Snehalatha Reddy <sabbidisnehalatha@gmail.com>
2025-11-22 13:05:52 +0530

    Refactor image prediction function with error handling
    Updated model loading and added error handling.

--- predict_image.py
+++ predict_image.py
@@ -3,20 +3,23 @@ import numpy as np
 from tensorflow.keras.models import load_model
 
 # Load the trained model
-model = load_model('breast_cancer_vgg16_model.h5')
+MODEL_PATH = 'breast_cancer_vgg16_model.keras'
+model = load_model(MODEL_PATH)
 
 def predict_image(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img) / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
+    try:
+        # Preprocess the image
+        img = image.load_img(img_path, target_size=(224, 224))
+        img_array = image.img_to_array(img) / 255.0
+        img_array = np.expand_dims(img_array, axis=0)
 
-    prediction = model.predict(img_array)
-    if prediction > 0.5:
-        return 'Malignant'
-    else:
-        return 'Benign'
+        # Make a prediction
+        prediction = model.predict(img_array)
+        confidence = prediction[0][0] * 100
+        if prediction > 0.5:
+            return f"Malignant "
+        else:
+            return f"Benign "
+    except Exception as e:
+        return f"Error during prediction: {str(e)}"
 
-# Usage
-img_path= r'C:\\Users\\SNEHA\\OneDrive\\Desktop\\Project1\\breast\\benign\\SOB\\adenosis\\SOB_B_A_14-29960CD\200X\\SOB_B_A-14-29960CD-200-004.png'  # Replace with actual image path
-result = predict_image(img_path)
-print(f'The image is: {result}')
